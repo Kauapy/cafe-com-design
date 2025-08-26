5946cdaca95f20f687ab6ff6a624b0f46851768c
Kauapy <kauadecarvalho20@gmail.com>
2025-08-26 20:13:21 -0300

    Criar header da página

---
+++ src/componentes/Header/header.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./header.css";
+import cafeteriaImg from "../../imagens/cafeteria.png";
+
+const header = () => {
+  return (
+    <div className="container-header">
+      <div>
+        <h3 className="titulo">Coffee with design</h3>
+        <img
+          src={cafeteriaImg}
+          alt="imagem cafeteria"
+          className="imagem-cafeteria"
+        />
+        <div className="subtitulo">
+          <h1>
+            Descubra cafés onde <br /> o design é tão bom <br /> quanto o café.
+          </h1>
+        </div>
+        <button
+          className="botao-explorar"
+          onClick={() => {
+            document
+              .getElementById("secao-cafes")
+              .scrollIntoView({ behavior: "smooth" });
+          }}
+        >
+          Explorar Cafés
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default header;
