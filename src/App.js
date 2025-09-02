5b6255ab0048e21513c980ad9696c09ad2fb8040
Kauapy <kauadecarvalho20@gmail.com>
2025-09-01 21:16:43 -0300

    Adicionar componente Footer e estilos correspondentes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './componentes/Header/header';
 import Main from './componentes/Main/Main';
+import Footer from './componentes/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Main></Main>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/componentes/Footer/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>
+        Feito por{" "}
+        <a
+          href="https://github.com/Kauapy"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Kauã
+        </a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
